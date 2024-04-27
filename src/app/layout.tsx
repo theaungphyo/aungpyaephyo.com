384a6b12df35628a222976d42ec77b31650aa57e
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-04-27 14:20:47 +0630

    feat: speedinsight and analytics ;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { cn, generateMetaData, montserrat } from '@/lib/utils';
 import Provider from '@/components/provider';
 import { Metadata } from 'next';
 import Navigation from '@/components/navigation';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = generateMetaData();
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -16,6 +17,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           'bg-white text-neutral-800 dark:bg-[rgb(5,5,5)] dark:text-neutral-200',
         )}
       >
+      <SpeedInsights />
+      <Analytics mode={'production'} />
         <Provider>
           <main
             className={cn(
