98846cae34a20b4debf2a34bca76f46e2f272a83
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-05-15 18:46:14 +0630

    refector: remove padding in template

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,11 +2,7 @@ import PageTransition from '@/components/page-transition';
 import { ReactNode } from 'react';
 
 const Template = ({ children }: { children: ReactNode }) => {
-  return (
-    <PageTransition className='pb-24 pt-16 md:pb-44 md:pt-8'>
-      {children}
-    </PageTransition>
-  );
+  return <PageTransition>{children}</PageTransition>;
 };
 
 export default Template;
