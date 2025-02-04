9f2b96cc4f64ea88e047d6df48f5d0440bd52569
Aung Pyae Phyo <devaungphyo@gmail.com>
2025-02-04 16:46:37 +0630

    feat: aptabase tracking added

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,18 +1,21 @@
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { BaseProps } from '@/types/base';
+import { AptabaseProvider } from '@aptabase/react';
 import { ThemeProvider } from 'next-themes';
 
 const Provider = ({ children }: BaseProps) => {
   return (
-    <ThemeProvider
-      disableTransitionOnChange
-      storageKey='theme'
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-    >
-      <TooltipProvider>{children}</TooltipProvider>
-    </ThemeProvider>
+    <AptabaseProvider appKey={process.env.APTABASE_API_KEY!}>
+      <ThemeProvider
+        disableTransitionOnChange
+        storageKey='theme'
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+      >
+        <TooltipProvider>{children}</TooltipProvider>
+      </ThemeProvider>
+    </AptabaseProvider>
   );
 };
 
