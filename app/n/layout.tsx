a33573b3eb58b987d049bbcb17d06eba52f1467b
Aung Pyae Phyo <aungphyo.dev@gmail.com>
2025-03-05 13:49:54 +0630

    feat: blog color invert

--- app/n/layout.tsx
+++ app/n/layout.tsx
@@ -3,7 +3,9 @@ import { PropsWithChildren } from 'react';
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <section className="container-wrapper pt-8">
-      <article className="container prose">{children}</article>
+      <article className="container prose dark:prose-invert">
+        {children}
+      </article>
     </section>
   );
 };
